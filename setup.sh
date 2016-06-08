a4a029245466db545ec1255d8509bef647ce0236
Ludovic ROBINOT <robinot@geovariances.com>
2016-06-08 13:55:16 +0200

    Check if java/unzip are installed

--- setup.sh
+++ setup.sh
@@ -1,26 +1,27 @@
-#!/bin/bash
-
-# if type -p java; then
-#     echo found java executable in PATH
-#     _java=java
-# elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-#     echo found java executable in JAVA_HOME
-#     _java="$JAVA_HOME/bin/java"
-# else
-#     echo "no java"
-# fi
-
-# if [[ "$_java" ]]; then
-#     version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-#     echo version "$version"
-#     if [[ "$version" > "1.5" ]]; then
-#         echo version is more than 1.5
-#     else
-#         echo version is less than 1.5
-#     fi
-# fi
+#!/usr/bin/env bash
 
 ACTIVATOR_VERSION=1.3.10
+
+if type -p java 2>&1 >/dev/null; then
+  _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+  _java="$JAVA_HOME/bin/java"
+else
+  sudo add-apt-repository --yes ppa:webupd8team/java
+  sudo apt-get update
+  sudo apt-get install --yes oracle-java8-installer
+fi
+
+if [[ "$_java" ]]; then
+  version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+  if [[ "$version" < "1.8" ]]; then
+    echo Only version 1.8+ is supported
+  fi
+fi
+
+if ! type -p unzip 2>&1 >/dev/null ; then
+  sudo apt-get install --yes unzip
+fi
 
 wget -q http://downloads.typesafe.com/typesafe-activator/${ACTIVATOR_VERSION}/typesafe-activator-${ACTIVATOR_VERSION}-minimal.zip
 unzip -q typesafe-activator-${ACTIVATOR_VERSION}-minimal.zip
