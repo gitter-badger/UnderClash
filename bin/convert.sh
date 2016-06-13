346d5c91d2501bd510b9fb400ff841506454d832
Ludovic ROBINOT <robinot@geovariances.com>
2016-06-13 15:20:54 +0200

    Add new formats and checks

--- bin/convert.sh
+++ bin/convert.sh
@@ -1,34 +1,43 @@
 #!/usr/bin/env bash
 
-SCRIPT_LOCATION=$0
-while [ -L "$SCRIPT_LOCATION" ]; do
-  SCRIPT_LOCATION=$(readlink -e "$SCRIPT_LOCATION")
-done
-TOP=$(dirname "$SCRIPT_LOCATION")
+command -v convert >/dev/null 2>&1 || { echo >&2 "Command \"convert\" required, but not installed.  Aborting."; exit 1; }
+command -v composite >/dev/null 2>&1 || { echo >&2 "Command \"composite\" required, but not installed.  Aborting."; exit 1; }
+command -v optipng >/dev/null 2>&1 || { echo >&2 "Command \"optipng\" required, but not installed.  Aborting."; exit 1; }
 
+TOP="$(readlink -f $( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..)"
+
+echo "Processing logo.eps to png[1024x1024] ..."
 convert -density 300 -resize "1024x1024" -colorspace SRGB -transparent white -flatten assets/logo.eps /tmp/$$.1.png
 convert /tmp/$$.1.png -fuzz 2% -transparent white /tmp/$$.2.png
 
-convert /tmp/$$.2.png -resize "512x512" public/img/favicon-512.png
-optipng -o 7 public/img/favicon-512.png
+echo "Processing png[1024x1024] to png[512x512] ..."
+convert /tmp/$$.2.png -resize "512x512" ${TOP}/public/img/logo-512.png
+optipng --quiet -o7 ${TOP}/public/img/logo-512.png
 
 # Touch icon for iOS 2.0+ and Android 2.1+
-convert /tmp/$$.2.png -resize "152x152" public/img/favicon-152.png
-optipng -o 7 public/img/favicon-152.png
+echo "Processing png[1024x1024] to png[152x152] ..."
+convert /tmp/$$.2.png -resize "152x152" ${TOP}/public/img/logo-152.png
+optipng --quiet -o7 ${TOP}/public/img/logo-152.png
 
 # IE 10 Metro tile icon (Metro equivalent of apple-touch-icon)
-convert /tmp/$$.2.png -resize "144x144" public/img/favicon-144.png
-optipng -o 7 public/img/favicon-144.png
+echo "Processing png[1024x1024] to png[144x144] ..."
+convert /tmp/$$.2.png -resize "144x144" ${TOP}/public/img/logo-144.png
+optipng --quiet -o7 ${TOP}/public/img/logo-144.png
 
 # IE 11 Tile for Windows 8.1 Start Screen
-convert /tmp/$$.2.png -resize "70x70" public/img/favicon-70.png
-optipng -o 7 public/img/favicon-70.png
-convert /tmp/$$.2.png -resize "150x150" public/img/favicon-150.png
-optipng -o 7 public/img/favicon-150.png
-convert /tmp/$$.2.png -resize "310x310" public/img/favicon-310.png
-optipng -o 7 public/img/favicon-310.png
+echo "Processing png[1024x1024] to png[70x70] ..."
+convert /tmp/$$.2.png -resize "70x70" ${TOP}/public/img/logo-70.png
+optipng --quiet -o7 ${TOP}/public/img/logo-70.png
 
+echo "Processing png[1024x1024] to png[150x150] ..."
+convert /tmp/$$.2.png -resize "150x150" ${TOP}/public/img/logo-150.png
+optipng --quiet -o7 ${TOP}/public/img/logo-150.png
+
+echo "Processing png[1024x1024] to png[310x310] ..."
+convert /tmp/$$.2.png -resize "310x310" ${TOP}/public/img/logo-310.png
+optipng --quiet -o7 ${TOP}/public/img/logo-310.png
+
+echo "Processing png[1024x1024] to png[310x150] ..."
 convert -size 310x150 xc:transparent /tmp/$$.3.png
-#convert /tmp/$$.3.png public/img/favicon-150.png -gravity center -geometry +80+0 public/img/favicon-310x150.png
-cp /tmp/$$.3.png public/img/favicon-310x150.png
-optipng -o 7 public/img/favicon-310x150.png
+composite -gravity center ${TOP}/public/img/logo-150.png /tmp/$$.3.png ${TOP}/public/img/logo-310x150.png
+optipng --quiet -o7 ${TOP}/public/img/logo-310x150.png
