5c944bd32cc7d517a9708d8f4453243566857897
Ludovic ROBINOT <robinot@geovariances.com>
2016-06-14 19:19:32 +0200

    Move import statement to the top of file

--- app/models/clashApi.scala
+++ app/models/clashApi.scala
@@ -1,5 +1,7 @@
 package models
 
+import play.api.libs.json.Json
+
 case class ApiImageUrls(tiny: Option[String], small: Option[String], medium: Option[String], large: Option[String])
 case class ApiLocation(id: Long, name: String, isCountry: Boolean, countryCode: String)
 case class ApiLeague(id: Long, name: String, iconUrls: ApiImageUrls)
@@ -7,8 +9,6 @@ case class ApiMember(tag: String, name: String, role: String, expLevel: Int, lea
 case class ApiClan(tag: String, name: String, `type`: String, description: String, location: ApiLocation, badgeUrls: ApiImageUrls, clanLevel: Int, clanPoints: Int, requiredTrophies: Int, warFrequency: String, warWinStreak: Int, warWins: Int, warTies: Int, warLosses: Int, isWarLogPublic: Boolean, members: Int, memberList: List[ApiMember])
 
 object JsonFormats {
-  import play.api.libs.json.Json
-
   // Generates Writes and Reads for Feed and User thanks to Json Macros
   implicit val apiImageUrlsFormat = Json.format[ApiImageUrls]
   implicit val apiLocationFormat = Json.format[ApiLocation]
