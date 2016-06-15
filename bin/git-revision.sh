beafe4e4391e5c11ea94283f95a160b41583cb79
Ludovic ROBINOT <robinot@geovariances.com>
2016-06-15 11:18:27 +0200

    Revision updater script

---
+++ bin/git-revision.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+projectversion=$(git describe --tags --long)
+vers1=$(echo ${projectversion} | cut -f1 -d- | sed 's/[^0-9.]//')
+vers3=$(echo ${projectversion} | cut -f2 -d-)
+vers4=$(echo ${projectversion} | cut -f3 -d-)
+
+vers2=$(git rev-list HEAD | wc -l)
+
+echo "${vers1}.${vers2}.${vers3}.r${vers4}"
