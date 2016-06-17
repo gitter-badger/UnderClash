75cd5dc47fe718e832a0a186f00998a7ccd09212
Ludovic ROBINOT <robinot@geovariances.com>
2016-06-17 18:09:29 +0200

    Lock early

--- bin/git-hooks.pre-push.sh
+++ bin/git-hooks.pre-push.sh
@@ -4,12 +4,13 @@ top=$(git rev-parse --show-toplevel)
 lock="${top}/.git-hooks.lock"
 if [ ! -e "${lock}" ];
 then
+  touch "${lock}"
+
   version=$("${top}/bin/git-revision.sh")
   date=$(date +"%d/%m/%Y %H:%M")
   echo "build.version=${version}" > "${top}/conf/version.conf"
   echo "build.date=\"${date}\"" >> "${top}/conf/version.conf"
 
-  touch "${lock}"
   git commit -m "Version update" "${top}/conf/version.conf"
   rm -f "${lock}"
 fi
