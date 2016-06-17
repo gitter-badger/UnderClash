f16fb8f383ef1b25b2cb6ea06a8c3ed5e39c0fc4
Ludovic ROBINOT <robinot@geovariances.com>
2016-06-17 16:08:27 +0200

    Do not commit version.conf after each commit

--- bin/git-hooks.post-commit.sh
+++ bin/git-hooks.post-commit.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
 top=$(git rev-parse --show-toplevel)
-lock="${top}/.post-commit.lock"
-if [ ! -e "${lock}" ];
-then
-  version=$("${top}/bin/git-revision.sh")
-  date=$(date +"%d/%m/%Y %H:%M")
-  echo "build.version=${version}" > "${top}/conf/version.conf"
-  echo "build.date=\"${date}\"" >> "${top}/conf/version.conf"
-
-  touch "${lock}"
-  git commit -m "Version update" "${top}/conf/version.conf"
-  rm -f "${lock}"
-fi
+version=$("${top}/bin/git-revision.sh")
+date=$(date +"%d/%m/%Y %H:%M")
+echo "build.version=${version}" > "${top}/conf/version.conf"
+echo "build.date=\"${date}\"" >> "${top}/conf/version.conf"
