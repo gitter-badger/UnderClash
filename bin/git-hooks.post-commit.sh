14ac5bb8a80a47d50dfd419598a7ee93fa4c916c
Ludovic ROBINOT <lrobinot@gmail.com>
2016-06-20 02:34:11 +0200

    Check lock in post-commit hook

--- bin/git-hooks.post-commit.sh
+++ bin/git-hooks.post-commit.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 top=$(git rev-parse --show-toplevel)
-version=$("${top}/bin/git-revision.sh")
-date=$(date +"%d/%m/%Y %H:%M")
-echo "build.version=${version}" > "${top}/conf/version.conf"
-echo "build.date=\"${date}\"" >> "${top}/conf/version.conf"
+lock="${top}/.git-hooks.lock"
+if [ ! -e "${lock}" ];
+then
+  version=$("${top}/bin/git-revision.sh")
+  date=$(date +"%d/%m/%Y %H:%M")
+  echo "build.version=${version}" > "${top}/conf/version.conf"
+  echo "build.date=\"${date}\"" >> "${top}/conf/version.conf"
+fi
