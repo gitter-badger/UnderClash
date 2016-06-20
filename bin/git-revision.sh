961ffc2ae9aa61792177b4e04ef52fb0297ef61c
Ludovic ROBINOT <lrobinot@gmail.com>
2016-06-20 02:43:10 +0200

    Double quote to prevent globbing and word splitting.

--- bin/git-revision.sh
+++ bin/git-revision.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 projectversion=$(git describe --tags --long)
-vers1=$(echo ${projectversion} | cut -f1 -d- | sed 's/[^0-9.]//')
-vers3=$(echo ${projectversion} | cut -f2 -d-)
-vers4=$(echo ${projectversion} | cut -f3 -d-)
+vers1=$(echo "${projectversion}" | cut -f1 -d- | sed 's/[^0-9.]//')
+vers3=$(echo "${projectversion}" | cut -f2 -d-)
+vers4=$(echo "${projectversion}" | cut -f3 -d-)
 
 vers2=$(git rev-list HEAD | wc -l)
 
